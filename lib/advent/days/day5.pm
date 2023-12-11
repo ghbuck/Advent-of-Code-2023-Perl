42cccdc7a8b1314f617377960bbcfe13afe908ff
Graham Buck <gbuck@bowdoin.edu>
2023-12-11 10:43:37 -0500

    first day5 commit

---
+++ lib/advent/days/day5.pm
@@ -0,0 +1,13 @@
+package Advent::Days::Day5;
+
+use v5.38.2;
+use warnings;
+use strict;
+use Data::Dumper;
+
+use Advent::Common;
+
+sub runDay {
+    my ($self, $questionNum, $doExample) = @_;
+    my $lines = getLines(5, $questionNum, $doExample);
+}
