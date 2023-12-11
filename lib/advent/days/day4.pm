2ee838b4ee5e249409a2c2440991725b1d1efeb6
Graham Buck <gbuck@bowdoin.edu>
2023-12-11 09:59:34 -0500

    rewrite day4 to remove time consuming loop

--- lib/advent/days/day4.pm
+++ lib/advent/days/day4.pm
@@ -3,15 +3,8 @@ package Advent::Days::Day4;
 use v5.38.2;
 use warnings;
 use strict;
-use feature qw(switch);
-    no warnings 'experimental';
 use Data::Dumper;
-
-use threads;
-use threads::shared;
-
-use FindBin qw($Bin);
-use lib "$Bin/lib";
+use List::Util qw(sum0);
 
 use Advent::Common;
 
@@ -21,21 +14,8 @@ sub runDay {
 
     my $total = 0;
 
-    # key => cardNum, value => numCards
-    my $winningCardData = {};
-    share($winningCardData);
-
-    # threading config
-    my $threadConfig = {};
-    share($threadConfig);
-
-    $threadConfig = {
-        maxThreads => 20,
-        numActive => 0
-    };
-
-    my $sharedQuestionNum :shared;
-    $sharedQuestionNum = $questionNum;
+    # index = cardId, value = numCards
+    my @numCardsData = (1)x(scalar @$lines);
 
     # process the file
     my $numLines = scalar @$lines;
@@ -43,47 +23,22 @@ sub runDay {
         my ($id, $winningNums, $nums) = ($1, $2, $3) if $line =~ m/^Card *(\d+): *(.+?) *\| *(.+?) *$/;
 
         if (defined $id and defined $winningNums and defined $nums) {
-            my %winningNums :shared;
-            %winningNums = map { $_, 1 } split(' ', $winningNums);
+            my %winningNums = map { $_, 1 } split(' ', $winningNums);
+            my @nums = split(' ', $nums);
 
-            my @nums :shared;
-            @nums = split(' ', $nums);
-
-            my $numCardsToProcess = 1 + ($winningCardData->{$id} ? $winningCardData->{$id} : 0);
-            print Dumper $threadConfig;
-#             for (my $iterator = 1; $iterator <= $numCardsToProcess; ++$iterator) {
-#                 while ($threadConfig->{numActive} == $threadConfig->{maxThreads}) {}
-#                 $threadConfig->{numActive}++;
-
-#                 threads->create(sub {
-#                     say Dumper $sharedQuestionNum;
-#                     # processCard(\$questionNum, \$numLines, \$id, \%winningNums, \@nums, $winningCardData, \$total);
-
-#                     # lock($threadConfig);
-#                     $threadConfig->{numActive}--;
-# # print Dumper $threadConfig;
-#                     threads->detach();
-#                 }, ($threadConfig, \$sharedQuestionNum, $numLines, $id, %winningNums, @nums, $winningCardData, $total));
-#             }
+            processCard(\$questionNum, \$numLines, \$id, \%winningNums, \@nums, \@numCardsData, \$total);
         }
     }
 
-    while ($threadConfig->{numActive} > 0) {}
-
     if ($questionNum == 2) {
-        # you start with 201 cards
-        $total = 201;
-
-        foreach my $numCards (values %$winningCardData) {
-            $total += $numCards;
-        }
+        $total = sum0(@numCardsData);
     }
 
     say "\n total: $total\n";
 }
 
 sub processCard {
-    my ($questionNum, $totalNumCards, $currentCardId, $winningNums, $nums, $winningCardData, $total) = @_;
+    my ($questionNum, $totalNumCards, $currentCardId, $winningNums, $nums, $numCardsData, $total) = @_;
 
     my $matches = 0;
 
@@ -94,23 +49,18 @@ sub processCard {
     }
 
     if ($matches > 0) {
-        given ($$questionNum) {
-            when (1) {
+        for ($$questionNum) {
+            if (/1/) {
                 my $score = 1 << ($matches - 1);
                 $$total += $score;
             }
-            when (2) {
+            elsif (/2/) {
+                my $multiplier = $numCardsData->[$$currentCardId];
                 for (my $iterator = 1; $iterator <= $matches; ++$iterator) {
                     my $wonCardId = $$currentCardId + $iterator;
 
                     if ($wonCardId <= $$totalNumCards) {
-                        lock($winningCardData);
-
-                        if (not defined $winningCardData->{$wonCardId}) {
-                            $winningCardData->{$wonCardId} = 1;
-                        } else {
-                            $winningCardData->{$wonCardId}++;
-                        }
+                        $numCardsData->[$wonCardId] += $multiplier;
                     }
                 }
             }
